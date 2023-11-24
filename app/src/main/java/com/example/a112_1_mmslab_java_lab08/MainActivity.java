7ecce7c59be546900f8076e632bd50cc1cac5242
s098031 <tests098031@gmail.com>
2023-11-24 14:22:12 +0800

    20231124

---
+++ app/src/main/java/com/example/a112_1_mmslab_java_lab08/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.a112_1_mmslab_java_lab08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, MyService.class);
+                startService(intent);
+                Toast.makeText(MainActivity.this, "Service ! 啟動 !!", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
